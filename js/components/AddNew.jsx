7838a628357eab155e73092d41a4a32b6a5513b0
notewrangler <noiseartist@gmail.com>
2015-11-28 10:33:20 -0800

    added paper to app.jsx

--- js/components/AddNew.jsx
+++ js/components/AddNew.jsx
@@ -1,2 +1,11 @@
-var React = require('react');
-var Backbone = require('../backbone-parse');
+// var React = require('react');
+// var Backbone = require('../backbone-parse');
+//
+//
+// module.exports = React.createClass({
+// 	render: function(){
+// 		return (
+//
+// 		)
+// 	}
+// })
--- js/components/App.jsx
+++ js/components/App.jsx
@@ -1,11 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+var Paper = require('material-ui/lib/paper');
 
 module.exports = React.createClass({
 	render: function() {
 		return (
-			<h1>todos</h1>
+			<div>
+				<h1>todos</h1>
+					<div id="notepad">
+						<Paper zDepth={4}>
+						  <p>zDepth=4</p>
+							<p>zDepth=4</p>
+							<p>zDepth=4</p>
+							<p>zDepth=4</p>
+						</Paper>
+					</div>
+			</div>
 		)
 	}
 });
