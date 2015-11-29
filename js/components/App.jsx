1c437372a72acee74b735869d8774b84dda246bf
notewrangler <noiseartist@gmail.com>
2015-11-29 01:29:18 -0800

    bottom edge of paper added

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Paper = require('material-ui/lib/paper');
 var AddNew = require('./AddNew.jsx');
+var Todo = require('./Todo.jsx');
 
 module.exports = React.createClass({
 	render: function() {
@@ -11,7 +12,14 @@ module.exports = React.createClass({
 					<div id="notepad">
 						<Paper zDepth={5}>
 						  <AddNew />
+							<Todo />
 						</Paper>
+						
+						<div id="bottom_edge">
+							<Paper className="layer1"></Paper>
+							<Paper className="layer2"></Paper>
+							<Paper className="layer3"></Paper>
+						</div>
 					</div>
 			</div>
 		)
--- js/components/Todo.jsx
+++ js/components/Todo.jsx
@@ -1,3 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Backbone = require('../backbone-parse');
+var Paper = require('material-ui/lib/paper');
+
+module.exports = React.createClass({
+	render: function() {
+		return (
+			<div>
+				
+			</div>
+		)
+	}
+})
