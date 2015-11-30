c6c5e5c5b1bada1bafd203e6db8ef79c78ff9f33
Jaimarie Velasco <jaimariev@Jaimaries-MacBook-Pro.local>
2015-11-29 23:27:00 -0800

    got it to save, but doesn't appear on page

--- js/components/list.jsx
+++ js/components/list.jsx
@@ -24,12 +24,63 @@ var listItem = Backbone.Model.extend({
 
 	lists.fetch({
 		success: function(resp) {
-			data = (resp.toJSON());
-			console.log('success ');
+			itemData = (resp.toJSON());
+			ReactDOM.render(<Sub itemData = {itemData} />, document.getElementById('subContainer'))
+			console.log('success ', resp);
 		},
 		error: function(err) {
 			console.log('err ');
-		}
+		},
 	});
 
-ReactDOM.render(<Sub data = {data} />, document.getElementById('subContainer'));
+var Sub = React.createClass({
+	render: function() {
+		var listData = this.props.itemData.map(function(obj){
+			return (
+				<table key={obj.itemData} className="list">{obj.myList}</table>
+			)
+		})
+		return (
+			<div>{listData}</div>
+		)
+	}
+});
+
+var ToDo = React.createClass({
+	_handleSubmit: function(e) {
+		e.preventDefault();
+		var newListItem = new listItem();
+
+		newListItem.set({
+			title: $("#newToDo").val()
+		})
+		title: $('#newToDo').val("");
+			newListItem.save(null, {
+				success: function(resp) {
+					console.log('success ', resp);
+					listCollection.fetch({
+						success: function(resp){
+						},
+						error: function(err){
+						}
+					})
+				},
+				error: function(err) {
+					console.log('err ');
+				} 
+			})
+	},
+	render: function(){
+		return (
+			<div>
+				<h1>To Do App</h1>
+				<form onSubmit={this._handleSubmit}>
+					<input id="newToDo" placeholder="What needs to be done?"/>
+				</form>
+			</div>
+		)
+	}
+});
+
+
+ReactDOM.render(<ToDo />, document.getElementById('mainContainer'));
--- js/main.jsx
+++ js/main.jsx
@@ -1,5 +1,4 @@
 require ('../css/main.css');
+require ('./components/list.jsx');
 var React = require ('react');
 var ReactDOM = require ('react-dom');
-
-
