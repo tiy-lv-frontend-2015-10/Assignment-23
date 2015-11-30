b27cfe6d3df3894e10caeeda091131867d594d20
Eijah Duane Prince <Prince@Eijahs-MacBook-Pro.local>
2015-11-29 22:12:26 -0600

    doner

--- end/src/script.js
+++ end/src/script.js
@@ -1,144 +1,141 @@
 var Todo = React.createClass({    
-  getInitialState: function() {
-        return {editing: false}
-    },    
-    edit: function() {
-              
-        this.setState({editing:true});
-        //console.log('Editing todo#' + this.props.index);      
+    getInitialState: function () {
+      return {editing:false}
     },
+    edit: function() {
+      this.setState({editing: true});
+    }, 
+
     remove: function() {
 
-        //alert('Todo removed');
-        this.props.onRemove(this.props.index);
+      this.props.onRemove(this.props.index);
     },
     save: function() {
 
-        var newValue = this.refs.newValue.getDOMNode().value;     
-        console.log(newValue + ' saved');
-        this.props.onChange(newValue,this.props.index);
-            
+        var val = this.refs.newValue.getDOMNode().value;
+        this.props.onChange(val, this.props.index);
         this.setState({editing: false});
     },
-    todoDisplay: function() {
+    todoDisplay:function() {
+
         return (
-            <div>
+                          
+          <li className="todo">
 
-                    <li className="todo">
+            <span onClick={this.edit}>
+              {this.props.children}
+            </span>
 
-                        <span onClick={this.edit}>
-                            {this.props.children}
-                        </span>
-
-                        <button onClick={this.remove} className="btn btn-default btn-sm glyphicon glyphicon-trash remove pull-right"/>
-
-
-                    </li>
-
-            </div>
-        );
-    }, 
-    todoForm: function() {
-        return (
-            <div>
-
-                    <li className="todo">
-
-                        <span>
-                            <input placeholder="Edit todo" type="text" ref="newValue" defaultValue={this.props.children}/>                             
-                        </span>
-
-                        <button onClick={this.save} className="btn btn-default btn-sm glyphicon glyphicon-floppy-disk pull-right"/>
+            <button onClick={this.remove} className="btn btn-default btn-sm glyphicon glyphicon-trash remove pull-right" />
 
 
-                    </li>
-         
+          </li>           
+            
+       );
+    },
+    todoForm:function() {
+      return (
+                          
+          <li className="todo">
 
-            </div>
-        );
+            <span>
+              <input type="text" placeholder="Edit Todo" ref="newValue" defaultValue={this.props.children} />
+            </span>
+
+            <button onClick={this.save} className="btn btn-default btn-sm glyphicon glyphicon-floppy-disk remove pull-right" />
+
+
+          </li>           
+            
+       );
 
     },
     render: function() {
       
-       if(this.state.editing){
-            return this.todoForm();
-       } else {
-            return this.todoDisplay();
-       }
-            
+      if(this.state.editing) {
+        return this.todoForm();
+      } else {
+        return this.todoDisplay();
+      }
+        
     },
       
 
-});//Todo
+});
+
+
+
 
 var TodoList = React.createClass({
 
-    getInitialState: function() {
-  
-        return {
-            todos: [
-                'Call Amy', 
-                'Pay phone bill', 
-                'Christmas cards'
-            ]
-        };
-    },
-    add: function() {
+  getInitialState: function() {
+    return {
 
-        var arr = this.state.todos;
-        var newTodo = this.refs.newTodo.getDOMNode().value;
-     
-        console.log('New todo: ' + newTodo);
-        arr.push(newTodo);
-        this.setState({todos: arr});        
-    },
-    update: function(newValue, i) {
+        todos: [
+          'Call Henry',
+          'Pay phone bill',
+          'Make dentist appt' 
 
-        var arr = this.state.todos;
-    
-        arr[i] = newValue;
-        this.setState({todos:arr});
-    },
-    remove: function(i) {
+        ]
+    };
+  },
+add:function(){
+  var arr =this.state.todos;
+  var newTodo = this.refs.newTodo.getDOMNode().value;
+  console.log('New Todo: ' + newTodo);
+  arr.push(newTodo);
+  this.setState({todos:arr});
+},
 
-        var arr = this.state.todos
+remove:function(){
+
+          var arr = this.state.todos
         arr.splice(i,1);
         this.setState({todos: arr})
         console.log('Todo#:' + (i+1) + ' removed');
 
-    }, 
-    eachTodo: function (todo,i) {
+},
+  update:function(newValue, i) {
 
-            return (
-                <Todo    key={i}
-                         index={i}
-                         onChange={this.update}
-                         onRemove={this.remove}>
-                        {todo}
-                </Todo>
-            );
-    },
-    render: function() {
+    var arr = this.state.todos;
+    arr[i] = newValue;
+    this.setState({todos: arr});
+  },
+  eachTodo: function(todo, i) {
+    return (
+      <Todo key={i}
+                 index={i}
+                 onChange={this.update}
+                 onRemove={this.remove}>
+              {todo}
+           </Todo>
+           );
+  },
+  render: function() {
 
-        return (
-            <div>
-                <h1>Things to DO</h1>
+    return  (
 
-                <div className="form-inline">
+        <div>
 
-                    <div className="form-group">
-                        <input className="form-control" placeholder="Add Todo" ref="newTodo"/>               
-                        <button onClick={this.add} className="btn btn-default btn-sm">+</button>             
-                    </div>
-             
-                </div>
+              <h1>Things to DO</h1>
 
-                <ul>
-                    {this.state.todos.map(this.eachTodo)}                 
-                </ul>
+              <div className="form-inline">
 
-            </div>
-        );
-    }
+                  <div className="form-group">
+                      <input ref="newTodo" className="form-control" placeholder='Add Todo' />
+                      <button onClick={this.add} className="btn btn-default btn-sm">+</button>
+                  </div>
+
+              </div>
+
+            <ul>
+              {this.state.todos.map(this.eachTodo)}
+            </ul>
+
+          </div>
+      );
+  }
+
 });
+
 React.render(<TodoList />, document.getElementById('todo'));
