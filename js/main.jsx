185cd1744ccd3b96891bafc6af076e20c358098a
noemiostorga <noemiostorga@gmail.com>
2015-12-01 21:45:20 -0800

    error list

--- js/main.jsx
+++ js/main.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Backbone = require('backbone');
-var List = require('../components/List.jsx');
+var List = require('/components/list.jsx');
 
 
-ReactDOM.render(<List />, document.getElementById('ListCom'));
+ReactDOM.render(<Input />, document.getElementById('ListCom'));
