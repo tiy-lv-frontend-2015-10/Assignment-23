4093835cdb721c941d3e59e80a6ea1679b6150bd
noemiostorga <noemiostorga@gmail.com>
2015-12-01 21:50:36 -0800

    from scratch

--- js/main.jsx
+++
@@ -1,7 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Backbone = require('backbone');
-var List = require('/components/list.jsx');
-
-
-ReactDOM.render(<Input />, document.getElementById('ListCom'));
