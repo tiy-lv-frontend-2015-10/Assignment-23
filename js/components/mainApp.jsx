ed39cdcd777971f0aa470630dad92ec41fea723b
noemiostorga <noemiostorga@gmail.com>
2015-12-02 07:58:40 -0800

    can't save

---
+++ js/components/mainApp.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Backbone = require('backbone');
+var ParseData = require('../backbone-parse.js');
+var Input = require('input.jsx');
+var ListForm = require('ListForm');
+
+var MainApp =require('')
